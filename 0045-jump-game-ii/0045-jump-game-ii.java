7742f04176f26e2af7ec3b2ec0ddaad2416b2f7e
Ayush Verma <155131149+ayushverma-cs@users.noreply.github.com>
2026-07-26 18:41:07 +0530

    Time: 2 ms (30.14%), Space: 47.4 MB (38.73%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int jump(int[] nums) {
+        int e=0;
+        int f=0;
+        int j=0;
+        for(int i=0;i<nums.length-1;i++){
+            f=Math.max(f,i+nums[i]);
+            if(i==e){
+                j++;
+                e=f;
+            }
+        }
+        return j;
+    }
+}
