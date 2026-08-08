c13c677e3fd302b9c76e70352dfffd0c84f4e242
Ayush Verma <155131149+ayushverma-cs@users.noreply.github.com>
2026-08-08 22:35:18 +0530

    Time: 8 ms (92.86%), Space: 45.9 MB (84.79%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -1,6 +1,11 @@
-public class Solution {
+class Solution {
     public boolean isPowerOfThree(int n) {
-        int maxPowerOf3 = 1162261467; 
-        return n > 0 && maxPowerOf3 % n == 0;
+        if (n <= 0) return false;
+
+        while (n % 3 == 0) {
+            n /= 3;
+        }
+
+        return n == 1;
     }
 }
