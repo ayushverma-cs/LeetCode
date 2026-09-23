e690416972690036e7b89fbad19a2cd5b43db4dc
Ayush Verma <155131149+ayushverma-cs@users.noreply.github.com>
2026-09-23 21:36:58 +0530

    Time: 1 ms (99.96%), Space: 42.6 MB (63.5%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int reverse(int x) {
+        int n =Math.abs(x);
+        int rev=0;
+        while(n!=0){
+            int ld=n%10;
+            if(rev>(Integer.MAX_VALUE-ld)/10){
+                return 0;
+
+            }
+            rev =rev*10+ld;
+            n=n/10;
+
+        }
+        return (x<0)? (-rev) : rev;      
+    }
+}
