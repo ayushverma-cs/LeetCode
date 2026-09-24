304d869123f101adb254f8f7636fd61f61f713f2
Ayush Verma <155131149+ayushverma-cs@users.noreply.github.com>
2026-09-25 00:45:59 +0530

    Time: 2 ms (97.64%), Space: 71.9 MB (98.86%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.java
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -1,34 +1,34 @@
 class Solution {
-    public boolean lemonadeChange(int[] bills) {
+    public boolean lemonadeChange(int[] b) {
         int f=0;
         int t=0;
-        for(int i=0;i<bills.length;i++){
-            if(bills[i]==5){
-                f++;
-            }
-            else if(bills[i]==10){
+        for(int i=0;i<b.length;i++){
+            if(b[i]==5) f++;
+            else if(b[i]==10){
                 if(f==0){
                     return false;
+                }else{
+                    f--;
+                    t++;
                 }
-                f--;
-                t++;
+                
             }
             else{
-                if(f>0 && t>0){
+
+                if(f>0&&t>0){
                     t--;
                     f--;
+                }else if(f>=3){
+                    f-=3;
                 }
-                else if (f >= 3) {
-                         f -= 3;
-                        }
-                else {
-                     return false;
-                    }
+                else{
+                    return false;
+                }
+                
             }
-        
-        
+
         }
         return true;
+    }
         
     }
-}
